39b0fa833b6f168351a6bdf329508675139ee2d2
kateyatsk <klaettoe2023@gmail.com>
2025-03-28 23:30:12 +0300

    Initial Commit

---
+++ PostsUICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsUICollectionView
+//
+//  Created by Екатерина Яцкевич on 28.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
